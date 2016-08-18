4da36ea37fee39e61f39da6cf71182e8318bff92
Geraldine Neal <Joe@geraldines-mac.local>
2016-08-17 21:06:48 -0400

    Initial Commit

---
+++ TSBB app 2Tests/TSBB_app_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TSBB_app_2Tests.swift
+//  TSBB app 2Tests
+//
+//  Created by Geraldine Neal on 8/17/16.
+//  Copyright © 2016 The Steel Buffalo Butchery. All rights reserved.
+//
+
+import XCTest
+@testable import TSBB_app_2
+
+class TSBB_app_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
